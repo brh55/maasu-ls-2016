4fa69c07492ebb2b4678ac0892a9ab121541a7ba
Brandon Him <brh55@drexel.edu>
2016-07-14 01:48:23 -0500

    Start of files

---
+++ src/_scripts/main.js
@@ -0,0 +1,11 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+var $ = require('jquery');
+
+$(function() {
+  new Link(); // Activate Link modules logic
+  console.log('Welcome to Yeogurt!');
+});
